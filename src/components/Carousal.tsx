81f9d71cb25f118f1184c8d225d7a6c5f783e991
Ankit Soni <ankitprahladsoni@gmail.com>
2021-07-05 17:06:53 -0700

    remove css

--- src/components/Carousal.tsx
+++ src/components/Carousal.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { useSpringCarousel } from "react-spring-carousel-js";
-import { css, Global } from "@emotion/react";
+import { css } from "@emotion/react";
 
 const Carousal = () => {
   const { carouselFragment } = useSpringCarousel({
@@ -40,7 +40,6 @@ const SliderItem = ({ children, ...rest }: any) => {
         width: 100%;
         height: 100%;
         font-size: 24px;
-        // color: #000;
         min-height: 200px;
         touch-action: none;
       `}
@@ -57,44 +56,12 @@ const SliderWrapper = ({ children }: any) => {
       css={css`
         border-radius: 12px;
         overflow: hidden;
-        // background-color: #fff;
         padding: 16px;
-        flex: 1;
         margin: 24px;
         box-shadow: 0 6.7px 5.3px rgba(0, 0, 0, 0.0012),
           0 12.3px 17.9px rgba(0, 0, 0, 0.062), 0 10px 20px rgba(0, 0, 0, 0.06);
       `}
     >
-      <Global
-        styles={css`
-          html {
-            display: flex;
-            position: absolute;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-          }
-          body {
-            display: flex;
-            flex: 1;
-            letter-spacing: 1px;
-            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-              Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
-              sans-serif;
-          }
-          #root {
-            display: flex;
-            flex: 1;
-            width: 100vw;
-          }
-          .sb-main-padded.sb-main-padded {
-            display: flex;
-            padding: 0;
-            margin: 0;
-          }
-        `}
-      />
       {children}
     </div>
   );
