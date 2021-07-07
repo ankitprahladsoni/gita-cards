7a66985561f9b7f78e6d98c6bee2596d0b36b982
Ankit Soni <ankitprahladsoni@gmail.com>
2021-07-07 01:52:10 -0700

    add types for commetary

--- src/components/util.ts
+++ src/components/util.ts
@@ -1,6 +1,6 @@
 import authors from "../data/authors.json";
 import chapters from "../data/chapters.json";
-import commentary from "../data/commentary.json";
+import * as commentary from "../data/commentary.json";
 import languages from "../data/languages.json";
 import translation from "../data/translation.json";
 import verses from "../data/verse.json";
@@ -9,6 +9,17 @@ type Verse = {
   text: string;
   translation: string;
   commentary: string;
+};
+
+type CM = {
+  authorName: string;
+  author_id: number;
+  description: string;
+  id: 11186;
+  lang: string;
+  language_id: number;
+  verseNumber: number;
+  verse_id: number;
 };
 export const getVerses = (): Verse[] => {
   return verses.map((v) => {
@@ -19,9 +30,9 @@ export const getVerses = (): Verse[] => {
           .filter((t) => t.verse_id === v.id)
           .find((t) => t.authorName === "Swami Ramsukhdas")?.description || "",
       commentary:
-        commentary
+        (commentary as CM[])
           .filter((c) => c.verse_id === v.id)
-          .find((c) => c.authorName === "Swami Ramsukhdas").description || "",
+          .find((c) => c.authorName === "Swami Ramsukhdas")?.description || "",
     };
   });
 };
