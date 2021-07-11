95cb4585eeedd44f53b7376f3fc09078f789abdf
Ankit Soni <ankitprahladsoni@gmail.com>
2021-07-11 14:43:27 -0700

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Carousel from "./components/Carousel";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Carousel />
+      <NavBar>
+        <Carousel />
+      </NavBar>
     </ChakraProvider>
   );
 }
