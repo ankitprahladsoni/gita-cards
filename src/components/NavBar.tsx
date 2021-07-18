446e4b280e12e10484caa848770e0fc4e6c08ac0
Ankit Soni <ankitprahladsoni@gmail.com>
2021-07-18 01:55:06 -0700

    cleanup

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,30 +1,5 @@
-import { ReactNode } from "react";
-import {
-  Box,
-  Flex,
-  HStack,
-  Link,
-  useColorModeValue,
-  Select,
-} from "@chakra-ui/react";
+import { Box, Flex, HStack, useColorModeValue, Select } from "@chakra-ui/react";
 import { useChapterContext } from "./ChapterContext";
-
-const Links = ["Dashboard", "Projects", "Team"];
-
-const NavLink = ({ children }: { children: ReactNode }) => (
-  <Link
-    px={2}
-    py={1}
-    rounded={"md"}
-    _hover={{
-      textDecoration: "none",
-      bg: useColorModeValue("gray.200", "gray.700"),
-    }}
-    href={"#"}
-  >
-    {children}
-  </Link>
-);
 
 export default function NavBar({ children }: any) {
   const [chapter, setChapter] = useChapterContext();
@@ -39,15 +14,6 @@ export default function NavBar({ children }: any) {
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
           <HStack spacing={8} alignItems={"center"}>
             <Box>Logo</Box>
-            <HStack
-              as={"nav"}
-              spacing={4}
-              display={{ base: "none", md: "flex" }}
-            >
-              {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
-              ))}
-            </HStack>
           </HStack>
           <Flex alignItems={"center"}>
             <Select
